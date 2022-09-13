841347e4b65747e91e3000cbb70ede379441efb0
Krzysztof Urban <krzysztof.urban@firma.interia.pl>
2022-09-13 21:43:05 +0200

    feat(front): Created classes and methods

---
+++ src/Circle.ts
@@ -0,0 +1,10 @@
+import { Options } from "./OptionFactory";
+
+export class Circle {
+  _option: string;
+  constructor(option: string) {
+    this._option = option;
+  }
+
+  onClick() {}
+}
---
+++ src/Cross.ts
@@ -0,0 +1,10 @@
+import { Options } from "./OptionFactory";
+
+export class Cross {
+  _option: string;
+  constructor(option: string) {
+    this._option = option;
+  }
+
+  onClick() {}
+}
